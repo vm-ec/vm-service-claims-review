c37a2f277fba7c6e021f556accbe54013fdb3064
Srivani <srivanivadthya@gmail.com>
2026-01-12 15:13:29 +0530

    Added AI Metrics

--- src/main/java/com/vm/service/claimsreview/controllers/ObservabilityController.java
+++ src/main/java/com/vm/service/claimsreview/controllers/ObservabilityController.java
@@ -1,11 +1,7 @@
 package com.vm.service.claimsreview.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import com.vm.service.claimsreview.service.ObservabilityService;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import com.vm.service.claimsreview.dto.MetricsResponseDTO;
+import com.vm.service.claimsreview.service.MetricsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,28 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/observe")
 @RequiredArgsConstructor
 public class ObservabilityController {
-    private final ObservabilityService observabilityService;
-	
-	@GetMapping("/health")
-    public ResponseEntity<Map<String, String>> healthCheck() {
+    private final MetricsService metricsService;
 
-        Map<String, String> response = new HashMap<>();
-        response.put("status", "UP");
-        response.put("message", "Service is running");
-
-        return ResponseEntity.ok(response);
+    @GetMapping("/metrics")
+    public ResponseEntity<MetricsResponseDTO> getMetrics() {
+        return ResponseEntity.ok(metricsService.getSampleData());
     }
-	
-	@GetMapping("/metrics")
-    public ResponseEntity<Map<String, String>> getMetrics() {
-        Map<String, String> map = observabilityService.getObservabilityContext();
-
-        Map<String, String> response = new HashMap<>();
-        response.put("status", "DOWN");
-        response.put("message", "Service is not reporting metrics");
-
-        return ResponseEntity.ok(map.get("metrics") != null ? map : response);
-    }
-	
-
 }
--- vm-service-claims-review-main/README.md
+++
@@ -1,2 +0,0 @@
-# vm-service-claims-review
-This is repo for review of the claims and approver/reject them
